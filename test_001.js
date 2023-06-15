678457a4c64fc9ab5b7f46dd8e3383010f40c2f3
jhforet <jh_foret@naver.com>
2023-06-16 00:04:25 +0900

    첫 번째 테스트:문자열 출력하기-수정

--- test_001.js
+++ test_001.js
@@ -1,14 +1,12 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const readline = require('readline'); // readline 모듈 불러오기
+const rl = readline.createInterface({ // readline 인터페이스 생성, 
+    input: process.stdin, // 표준 입력
+    output: process.stdout // 표준 출력
 });
 
-let input = ["HelloWorld!"];
-
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-    console.log(str)
+rl.on('line', function (line) { // 'line' 이벤트 리스너를 등록, 입력한 데이터가 한 줄로 들어올 때마다 호출
+    input = [line]; // 입력된 데이터를 배열에 할당
+}).on('close',function(){ // 'close' 이벤트 리스너를 등록, 입력이 종료되면 호출
+    str = input[0]; // 배열에 할당된 입력 데이터를 변수에 저장
+    console.log(str) // 변수에 저장된 값을 출력
 });
